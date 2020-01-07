69152c0e899588c3c3a2221f8e2bc48cc9d02d8e
Aaronvan <1421589745@qq.com>
2020-01-07 17:21:13 +0800

    增加umirc文件

--- .umirc.js
+++ .umirc.js
@@ -1,19 +1,20 @@
-
 // ref: https://umijs.org/config/
+import { resolve } from 'path'
+
 export default {
   disableCSSModules: true,
   treeShaking: true,
-  routes: [
-    {
-      path: '/',
-      component: '../layouts/index',
-      routes: [
-        { path: '/', component: '../pages/index' },
-        { path: '/login', component: '../pages/login' },
-        { path: '/detail', component: '../pages/detail' }
-      ]
-    },
-  ],
+  // routes: [
+  //   {
+  //     path: '/',
+  //     component: '../layouts/index',
+  //     routes: [
+  //       { path: '/', component: '../pages/index' },
+  //       { path: '/login', component: '../pages/login' },
+  //       { path: '/detail', component: '../pages/detail' }
+  //     ]
+  //   },
+  // ],
   plugins: [
     // ref: https://umijs.org/plugin/umi-plugin-react.q
     ['umi-plugin-react', {
@@ -34,4 +35,9 @@ export default {
       },
     }],
   ],
+  alias: {
+    api: resolve(__dirname, './src/services/'),
+    components: resolve(__dirname, './src/components'),
+    assets: resolve(__dirname, './src/assets'),
+  },
 }
